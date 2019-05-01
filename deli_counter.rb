ed1a2ad392685893a7b63b411b8e0967aee79a4d
Aliya Lewis <aliyalewis5@gmail.com>
2019-05-01 01:00:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,11 +13,10 @@ def line(katz_deli)
     end
 end
 
-def take_a_number(name, line)
-line = line_count
+def take_a_number(name)
   line_count = 0
   while line_count > 0
-    return "Welcome, #{name}. You are number #{line} in line."
+    return "Welcome, #{name}. You are number #{line_count} in line."
     line_count += 1
   end
 end
