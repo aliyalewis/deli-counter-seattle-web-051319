ab26f79e936e2056671d89445a966e49caf52280
Aliya Lewis <aliyalewis5@gmail.com>
2019-05-01 00:54:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,13 @@
+require "pry"
+
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    line = "The line is currently: "
-    katz_deli.each.with_index(1) do |number, name|
-      line << "#{number}. #{name} "
+    line = "The line is currently:"
+    katz_deli.each.with_index(1) do |name, number|
+      # binding.pry
+      line << " #{number}. #{name}"
       end
       puts line
     end
