b29c0a52434bbdd46213835d2d6262a9eef87b41
Aliya Lewis <aliyalewis5@gmail.com>
2019-05-01 01:04:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,5 +15,5 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.count}n in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
 end
