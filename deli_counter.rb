980a77920a12fb1ce111c4b9d5b932bc3fc05ba4
Aliya Lewis <aliyalewis5@gmail.com>
2019-05-01 01:09:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,11 @@ def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
 end
+
+def now_serving(katz_deli, name)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+  end
+end
